ee8e2e8477d0c2f39d2b1534675bc78be89b638d
kaganomer956-ctrl <kaganomer956@gmail.com>
2026-07-03 10:50:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,64 @@ card.style.display="none";
 search.addEventListener("keyup",updateVideos);
 
 filter.addEventListener("change",updateVideos);
+// LIKE & WATCH
+
+const likeButtons=document.querySelectorAll(".like-btn");
+
+likeButtons.forEach(btn=>{
+
+let liked=false;
+
+btn.addEventListener("click",()=>{
+
+const span=btn.querySelector(".like-count");
+
+let count=parseInt(span.innerText);
+
+if(!liked){
+
+count++;
+
+liked=true;
+
+btn.style.background="#ff2b2b";
+
+}else{
+
+count--;
+
+liked=false;
+
+btn.style.background="#1a1a1a";
+
+}
+
+span.innerText=count;
+
+});
+
+});
+
+const videos=document.querySelectorAll("video");
+
+videos.forEach(video=>{
+
+const card=video.closest(".video-card");
+
+if(!card) return;
+
+const watch=card.querySelector(".watch-count");
+
+let viewed=false;
+
+video.addEventListener("play",()=>{
+
+if(viewed) return;
+
+viewed=true;
+
+watch.innerText=parseInt(watch.innerText)+1;
+
+});
+
+});
